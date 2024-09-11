971d004c70abf13ccc7eb891f573c30c74627fe1
marcioflaviob <mbrandao@student.42.fr>
2024-09-11 21:22:52 +0200

    Implemented Request class

--- Request.cpp
+++ Request.cpp
@@ -6,9 +6,56 @@
 /*   By: mbrandao <mbrandao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 20:56:47 by mbrandao          #+#    #+#             */
-/*   Updated: 2024/09/11 20:56:50 by mbrandao         ###   ########.fr       */
+/*   Updated: 2024/09/11 21:21:05 by mbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
 
+Request::Request() {
+	
+}
+
+Request::~Request() {
+
+}
+
+std::string Request::getBody() {
+	return _body;
+}
+
+std::string Request::getPath() {
+	return _path;
+}
+
+RequestType Request::getType() {
+	return _type;
+}
+
+std::map<std::string, std::string> Request::getHeaders() {
+	return _headers;
+}
+
+std::string Request::getHeader(std::string key) {
+	std::map<std::string, std::string>::iterator it = _headers.find(key);
+	if (it == _headers.end()) {
+		throw KeyNotFound();
+	}
+	return it->second;
+}
+
+void Request::setBody(std::string body) {
+	_body = body;
+}
+
+void Request::setPath(std::string path) {
+	_path = path;
+}
+
+void Request::setType(RequestType type) {
+	_type = type;
+}
+
+void Request::addHeader(std::string key, std::string value) {
+	_headers[key] = value;
+}
