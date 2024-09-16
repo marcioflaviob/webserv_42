c3bbac18792cddddf068f42edf54f1a5dc2d716c
svetlana <svydrina@student.42.fr>
2024-09-16 16:55:53 +0200

    added a python file

---
+++ cgi-bin/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print ("Content-type:text/html\r\n\r\n")
+print('<html>')
+print('<head>')
+print('<title>Hello World - This is a CGI program</title>')
+print('</head>')
+print('<body>')
+print('<h2>Hello World! Look at our fabulous CGI program</h2>')
+print('</body>')
+print('</html>')
+
