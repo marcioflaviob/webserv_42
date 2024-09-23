692ccc9e3adc2fe09eb07d97e6cce6a410e4c5fc
svetlaan <nyagalen@gmail.com>
2024-09-24 00:37:08 +0200

    added an html and a python file. the html works just fine, but the pyhon one is somewhat troubled

---
+++ cgi-bin/hello_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import cgi, cgitb
+
+form = cgi.FieldStorage()
+
+first_name = form.getvalue('first_name')
+last_name = form.getvalue('last_name')
+
+print ("Content-type:text/html")
+print()
+print("<html>")
+print ('<head>')
+print ("<title>Hello - Second CGI Program</title>")
+print ('</head>')
+print ('<body>')
+print ("<h2>Hello %s %s</h2>" % (first_name, last_name))
+print ('</body>')
+print ('</html>')
